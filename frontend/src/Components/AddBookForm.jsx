00f9076c95f03d4025b00829367b97b145ce8f0b
gourav <gouravsharma20a@gmail.com>
2026-06-19 16:47:37 +0530

    feature: i have linked my backend with frontend

---
+++ frontend/src/Components/AddBookForm.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react"
+
+const AddBookForm = () => {
+
+    const [formData,setFormData] = useState({
+        title:"",
+        author:"",
+        publishedYear:"",
+        genre:"",
+        language:"",
+        country:"",
+        rating:"",
+        summary:"",
+        coverImageUrl:""
+    })
+
+    const handleChange = (e) => {
+        const {name,value} = e.target
+        setFormData((prevState) => ({
+            ...prevState,
+            [name] : name === "publishedYear" || name === "rating" ? parseInt(value) :value
+        }));
+    }   
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+
+        console.log(formData)
+
+        try {
+            const response = await fetch("https://be-4-assignment1-navy.vercel.app/books",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(formData)})
+
+            if (!response.ok) {
+                console.log("Status:", response.status)
+                const errorBody = await response.text()
+                console.log("Error body:", errorBody)
+                throw new Error("Failed to add Books")
+            } 
+
+            const data = await response.json()
+
+            console.log("Added movie",data)
+
+        } catch(err) {
+            console.log(err)
+
+        }
+
+    }
+    
+    return (
+        <div>
+        <h2>Add New Movie</h2>
+        <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input type="text" name="title" value={formData.title} onChange = {handleChange}/>
+            <br/>
+            <br/>
+
+
+
+            <label>Author</label>
+            <input type="text" name="author" value={formData.author} onChange={handleChange}/>
+            <br/>
+            <br/>
+
+
+            <label>Publish Year</label>
+            <input type="number" name="publishedYear" value={formData.publishedYear} onChange={handleChange}/>
+            <br/>
+            <br/>
+
+
+            <label>Genre</label>
+            <input type="text" name="genre" value={formData.genre} onChange={handleChange}/>
+            <br/>
+            <br/>
+
+
+
+
+            <label>Language</label>
+            <input type="text" name="language" value={formData.language} onChange={handleChange}/>
+            <br/>
+            <br/>
+
+
+            <label>Country</label>
+            <input type="text" name="country" value={formData.country} onChange={handleChange}/>
+            <br/>
+            <br/>
+
+
+            <label>Rating</label>
+            <input type="number" name="rating" value={formData.rating} onChange={handleChange}/>
+            <br/>
+            <br/>
+
+
+
+            <label>Summary</label>
+            <input type="text" name="summary" value={formData.summary} onChange={handleChange}/>
+            <br/>
+            <br/>
+
+
+
+
+            <label>cover Image Url</label>
+            <input type="text" name="coverImageUrl" value={formData.coverImageUrl} onChange={handleChange}/>
+            <br/>
+            <br/>
+
+
+
+            <button>Submit</button>
+        </form>
+        </div>
+
+    )
+
+
+}
+
+export default AddBookForm
---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import AddBookForm from './Components/AddBookForm.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <AddBookForm/>
+)
